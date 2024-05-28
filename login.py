d60163129f988689aeb4fc57941a2e7cdad8cde0
Abdulaziz <shukurov@gmail.ru>
2024-05-28 14:45:03 +0500

    click_application_project

---
+++ login.py
@@ -0,0 +1,12 @@
+import classes
+
+
+def login():
+    """Bu yerda foydalanuvchi uzining pin kodi orqali kirish joyini tekshiradi"""
+    print("Welcome Login display ")
+    pin = int(input("pincod : "))
+    if classes.User.check_user(pin):
+        return classes.User.check_user
+    else:
+        print("Error")
+        return login()
